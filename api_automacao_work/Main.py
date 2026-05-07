6e5801f028b49b3ce736f9789c434acf05dabc94
Ruan <ruanfontes309@gmail.com>
2026-05-07 11:12:56 -0300

    teste: feito teste de token

--- api_automacao_work/Main.py
+++ api_automacao_work/Main.py
@@ -1,10 +1,23 @@
 from dotenv import load_dotenv
 import os
+import webbrowser
 
 load_dotenv()
 
-app_id = os.getenv("APP_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+APP_ID = os.getenv("APP_ID")
 
-print("APP_ID:", app_id)
-print("CLIENT_SECRET:", client_secret)
+redirect_uri = "https://google.com"
+
+auth_url = (
+    f"https://auth.mercadolivre.com.br/authorization"
+    f"?response_type=code"
+    f"&client_id={APP_ID}"
+    f"&redirect_uri={redirect_uri}"
+)
+
+print("Abrindo navegador...")
+
+webbrowser.open(auth_url)
+
+print("\nSe não abrir automaticamente:")
+print(auth_url)
